f1e04b5e62fe50de1092774003e206b60c00bde5
Jon-nim <jonathan.morgan.computer@gmail.com>
2022-12-12 22:26:38 -0700

    3 files modified

--- public/script.js
+++ public/script.js
@@ -4,6 +4,7 @@ $(document).ready(() => {
   $(document).on('change', '#course', unhide);
   $(document).on('keyup', '#uvuId', cleantext);
   $(document).on('keyup', '#uvuId', fetchUVUData);
+  $(document).on('submit', '#form', postUVUdata);
 });
 
 //unhides when a course is selected
@@ -34,7 +35,7 @@ function hideComment(element) {
 
 function fetchCourseData(event) {
   // URL for the database, specifying courses
-  let url = `https://json-server-gupuqp--3000.local.webcontainer.io/api/v1/courses`;
+  let url = `http://localhost:3000/api/v1/courses`;
 
   // Set uvuId search bar placeholder here
   $('#uvuId').attr('placeholder', '10234567');
@@ -67,7 +68,7 @@ function fetchUVUData(event) {
 
   if ($('#uvuId').val().length == 8) {
     // get request for logs with specified uvuid and courseId
-    let url = `https://json-server-gupuqp--3000.local.webcontainer.io/api/v1/logs?uvuId=${uvuId}&courseId=${corseId}`;
+    let url = `http://localhost:3000/api/v1/logs?uvuId=${uvuId}&courseId=${corseId}`;
 
     axios
       .get(url)
@@ -86,4 +87,37 @@ function fetchUVUData(event) {
       })
       .catch((err) => console.log(err));
   }
+}
+
+function postUVUdata(event) {
+  event.preventDefault();
+  const uvuId = $('#uvuId').val();
+  const courseId = $('#course').val();
+  const postlogs = $('#textareaLog').val();
+  let date = new Date();
+  date = date.toLocaleString();
+  const id = createUUID();
+  let params = {
+    courseId: courseId,
+    uvuId: uvuId,
+    date: date,
+    text: postlogs,
+    id: id,
+  };
+
+  let url = `http://localhost:3000/api/v1/logs`;
+  axios.post(url, params).then(function (response) {
+    console.log(response);
+    $('#textareaLog').val('');
+    fetchUVUData();
+    $('button[data-cy="add_log_btn"]').prop('disabled', true);
+  });
+}
+
+function createUUID() {
+  return 'xxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
 }
